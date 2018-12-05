4632515623523ca23daf791468a3caf0aca0335a
Joshua French <jbf1001@gmail.com>
2018-12-05 15:06:00 -0600

    Removing hyde, adding easy-jekyll, reorganizing lisp folder and config loads for that folder.

---
+++ elpa/easy-jekyll-20181202.945/easy-jekyll-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "easy-jekyll" "20181202.945" "Major mode managing jekyll blogs" '((emacs "24.4")) :commit "5ee52c0bb01336a03a8f07e072841caf13f86c0a" :authors '(("Masashı Mıyaura")) :maintainer '("Masashı Mıyaura") :url "https://github.com/masasam/emacs-easy-jekyll")
--- elpa/hyde-20160508.308/hyde-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; hyde-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("hyde-git.el" "hyde-md.el" "hyde-pkg.el"
-;;;;;;  "hyde.el" "sample-dot-hyde.el") (23550 60968 318956 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; hyde-autoloads.el ends here
